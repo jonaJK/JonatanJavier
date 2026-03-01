94eff04f81fae1e9f4cef6007c34d53e352e3909
JonaJK <jona012311@gmail.com>
2026-03-01 01:09:38 -0600

    feat: add header and modal to layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './core/layout/header/header';
+import { ConfirmModal } from '@shared/components/confirm-modal/confirm-modal';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, Header, ConfirmModal],
+    templateUrl: './app.html',
+    styleUrl: './app.css',
 })
 export class App {}
