60013c3dcb5314b7b5b097d70dfd1ab2793d6d11
JonaJK <jona012311@gmail.com>
2026-03-01 00:40:25 -0600

    refactor: add absolute path to jest config

--- jest.config.ts
+++ jest.config.ts
@@ -4,5 +4,10 @@ const config: Config = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
   testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/dist/'],
+  moduleNameMapper: {
+    '^@core/(.*)$': '<rootDir>/src/app/core/$1',
+    '^@shared/(.*)$': '<rootDir>/src/app/shared/$1',
+	'^@features/(.*)$': '<rootDir>/src/app/features/$1',
+  },
 };
 export default config;
